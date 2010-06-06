cbb4c8642cfbf0dbcbb64005360933b1290cb550
Erik Gregg <ralree@gmail.com>
2010-06-06 17:55:43 -0400

    Addition and subtraction work

--- school/605.404.OO.Programming.With.Cpp/HW4/hugeint.h
+++ school/605.404.OO.Programming.With.Cpp/HW4/hugeint.h
@@ -1,5 +1,11 @@
 #include <string>
 using std::string;
+#include <locale>
+using std::isdigit;
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <algorithm>
 #include <stdint.h>
 class HugeInt
 {
@@ -11,7 +17,9 @@ class HugeInt
       this->integerFieldSize = 50;
       this->integerField = new uint8_t[this->integerFieldSize];
       // Set to 255 (denotes empty)
-      memset(this->integerField, 255, this->integerFieldSize);
+      memset(this->integerField, 255, this->integerFieldSize - 1);
+      // Set last value to 0
+      this->integerField[this->integerFieldSize] = 0;
     }
 
     // 1 or 2 parameter constructor
@@ -22,18 +30,51 @@ class HugeInt
       this->integerField = new uint8_t[this->integerFieldSize];
       // Set to 255 (denotes empty)
       memset(this->integerField, 255, this->integerFieldSize);
+      // Set last value to 0
+      this->integerField[this->integerFieldSize - 1] = 0;
       init(i);
     }
 
+    // 1 or 2 parameter constructor
+    // Default size is 50
+    HugeInt(string s)
+    {
+      // XXX: Throw exception if s.length() > size
+      this->integerFieldSize = s.size() + 1;
+      this->integerField = new uint8_t[this->integerFieldSize];
+      // Set to 255 (denotes empty)
+      memset(this->integerField, 255, this->integerFieldSize);
+      // Set last value to 0
+      this->integerField[this->integerFieldSize - 1] = 0;
+      init(s);
+    }
+
     // Copy constructor
-    HugeInt(HugeInt& other)
+    HugeInt(const HugeInt& from)
     {
       // Create space for new data
-      this->integerFieldSize = other.integerFieldSize;
-      this->integerField = new uint8_t[this->integerFieldSize];
+      integerFieldSize = from.integerFieldSize;
+      integerField = new uint8_t[integerFieldSize];
       // Copy other data
-      memcpy(this->integerField, other.integerField, 
-             this->integerFieldSize);
+      memcpy(integerField, from.integerField, integerFieldSize);
+    }
+
+    // Initialize the instance with a string
+    // Expects a clean instance!
+    void init(string s)
+    {
+      // Fill in integerField
+      int offset = this->integerFieldSize - s.length();
+      for(int j = 0; j < s.length(); j++)
+      {
+        // Add the characters
+        if(isdigit(s[j]))
+        {
+          // Fill in with non-ASCII value of numeral
+          this->integerField[offset + j] = s[j] - 0x30;
+        }
+        // XXX: else throw exception
+      }
     }
 
     // Initialize the instance with an integer
@@ -55,39 +96,137 @@ class HugeInt
       }
     }
 
-    // HugeInt assignment operator
-    void operator = (const HugeInt& right) 
+    // HugeInt equality operator
+    bool operator == (const HugeInt& right) const
     {
-      // Groom space for new data
-      delete[] this->integerField;
-      this->integerFieldSize = right.integerFieldSize;
-      this->integerField = new uint8_t[this->integerFieldSize];
-      // Copy the entire integer field
-      memcpy(this->integerField, 
-             right.integerField, this->integerFieldSize);
+      return true;
+    }
+
+    // HugeInt string equality operator
+    bool operator == (const string& right) const
+    {
+      return true;
     }
 
     // Integer assignment operator
-    void operator = (const int& right) 
+    HugeInt operator = (const int& right) 
     {
       // Groom space for new data
       memset(this->integerField, 255, this->integerFieldSize);
       // Initialize
       init(right);
+      return *this;
     }
 
-    // uint64_t cast operator
-    operator uint64_t()
+    // Integer addition
+    HugeInt operator + (const int& right) 
     {
-      uint64_t integerValue = 0;
-      int j = 0;
-      int multiplier = 1;
-      for(j = this->integerFieldSize; j > 0; j--)
+      return *this + HugeInt(right);
+    }
+
+    // HugeInt addition
+    HugeInt operator + (const HugeInt& right) 
+    {
+      const HugeInt* to_add;
+      HugeInt dest;
+      if(right.size() > size())
       {
-        integerValue += this->integerField[j] * multiplier * 10;
-        multiplier *= 10;
+        // Right side is bigger - Copy it
+        dest = HugeInt(right);
+        to_add = this;
       }
-      return integerValue;
+      else
+      {
+        // Left is bigger or sizes are equal.  Copy this
+        dest = HugeInt(*this);
+        to_add = &right;
+      }
+      // From the end of the integers, add and carry
+      uint8_t result;
+      for(int i = 1; i <= to_add->size(); i++)
+      {
+        result = to_add->integerField[to_add->integerFieldSize - i];
+        result += dest.integerField[dest.integerFieldSize - i];
+        if(result > 9) 
+        {
+          // Check for overflow
+          if(i + 1 == dest.integerFieldSize)
+          {
+            // XXX: Exception
+            return HugeInt(0);
+          }
+          else
+          {
+            // Set previous value to 1 if it's uninitialized.
+            if(255 == dest.integerField[dest.integerFieldSize - i - 1])
+            {
+              dest.integerField[dest.integerFieldSize - i - 1] = 1;
+            }
+            else
+            {
+              // Increment previous digit
+              dest.integerField[dest.integerFieldSize - i - 1]++;
+            }
+          }
+        }
+        dest.integerField[dest.integerFieldSize - i] = result % 10;
+      }
+      return dest;
+    }
+
+    // HugeInt subtraction
+    HugeInt operator - (const HugeInt& right) 
+    {
+      HugeInt to_return = HugeInt(*this);
+      // From the end of the integers, add and carry
+      int8_t result;
+      for(int i = 1; i < right.size() + 1; i++)
+      {
+        result = to_return.integerField[to_return.integerFieldSize - i];
+        result -= right.integerField[right.integerFieldSize - i];
+        if(result < 0) 
+        {
+          // Decrement previous value
+          // If uninitialized, we've gone negative.
+          if(255 == to_return.integerField[to_return.integerFieldSize - i - 1])
+          {
+            return HugeInt(0);
+          }
+          else
+          {
+            // Loop until we decrement something
+            int j;
+            for(j = to_return.integerFieldSize - i - 1;
+                j > 0; j--)
+            {
+              // If we hit 255, we've underflowed
+              if(to_return.integerField[j] == 255)
+              {
+                return HugeInt(0);
+              }
+              // If we hit a 0, just change to a 9 and keep iterating
+              if(to_return.integerField[j] == 0)
+              {
+                to_return.integerField[j] = 9;
+              }
+              else
+              {
+                // Otherwise, just decrement and break.
+                --to_return.integerField[j];
+                break;
+              }
+            }
+            // Make sure we haven't depleted the array
+            if(0 == j)
+            {
+                return HugeInt(0);
+            }
+            result += 10;
+          }
+        }
+        to_return.integerField[to_return.integerFieldSize - i] = result % 10;
+      }
+      return to_return;
     }
 
     // String cast operator
@@ -96,7 +235,7 @@ class HugeInt
       // String to be built
       string s;
       // Calculate the offset to start with
-      int offset = this->integerFieldSize - size() + 1;
+      int offset = this->integerFieldSize - size();
       for(int i = offset; i < this->integerFieldSize; i++)
       {
         // Just append the ASCII value
@@ -105,17 +244,18 @@ class HugeInt
       return s;
     }
 
-    int size()
+    int size() const
     {
-      int i = this->integerFieldSize;
+      // We know the last number must be filled in.
+      int i = this->integerFieldSize - 2;
       // Iterate backwards until we find a zero
-      while(i > 0 && this->integerField[i] != 0)
+      while(i > 0 && this->integerField[i] != 255)
       {
         i--;
       }
       // Whether we reached index 0 or stopped early,
       // still return the distance we travelled
-      return this->integerFieldSize - i;
+      return this->integerFieldSize - i - 1;
     }
 
   private:
--- school/605.404.OO.Programming.With.Cpp/HW4/hugeint_unittest.cpp
+++ school/605.404.OO.Programming.With.Cpp/HW4/hugeint_unittest.cpp
@@ -4,36 +4,88 @@
 using std::string;
 #include "hugeint.h"
 
-TEST(HugeInt, IntCast)
-{
-  HugeInt i(1032423124);
-  EXPECT_EQ(1032423124, (int)i);
-}
-
-TEST(HugeInt, Negative)
-{
-  ASSERT_ANY_THROW(HugeInt i(-1033124));
-}
-
-TEST(HugeInt, Zero)
-{
-  HugeInt i(0);
-  ASSERT_EQ(0, (int)i);
-}
-
-TEST(HugeInt, Size)
-{
-  HugeInt i(32423432);
-  ASSERT_EQ(8, i.size());
-  //HugeInt j(0);
-  //ASSERT_STREQ("0", (char *)i);
-}
-
 TEST(HugeInt, StringCast)
 {
   HugeInt i(32423432);
   string s = (string)i;
   ASSERT_STREQ("32423432", s.c_str());
-  //HugeInt j(0);
-  //ASSERT_STREQ("0", (char *)i);
+  HugeInt j(0);
+  s = (string)j;
+  ASSERT_STREQ("0", s.c_str());
+  HugeInt k("50003000800060004000");
+  s = (string)k;
+  ASSERT_STREQ("50003000800060004000", s.c_str());
+}
+
+TEST(HugeInt, StringAssignmentAndEquality)
+{
+  HugeInt i = string("7");
+  ASSERT_STREQ(((string)i).c_str(), "7");
+}
+
+TEST(HugeInt, Negative)
+{
+  HugeInt i(-1033124);
+  string s = (string)i;
+  EXPECT_STREQ("1033124", s.c_str());
+}
+
+TEST(HugeInt, Zero)
+{
+  HugeInt i(0);
+  string s = (string)i;
+  ASSERT_STREQ("0", s.c_str());
+}
+
+TEST(HugeInt, IntAssignment)
+{
+  HugeInt i = 7;
+  ASSERT_STREQ(((string)i).c_str(), "7");
+}
+
+TEST(HugeInt, HugeIntAssignment)
+{
+  HugeInt i("12345678912345678900055534200112393811234");
+  HugeInt j = i;
+  ASSERT_STREQ("12345678912345678900055534200112393811234", ((string)j).c_str());
+}
+
+TEST(HugeInt, InitializationOverflow)
+{
+  HugeInt m("12345678912345678900055534200112393811234000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+  HugeInt n("83947238473208947234723984732849023784023000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+}
+
+TEST(HugeInt, Addition)
+{
+  HugeInt i("12345678912345678900055534200112393811234");
+  HugeInt j("83947238473208947234723984732849023784023");
+  HugeInt l = (i + j);
+  ASSERT_STREQ("12345678912345678900055534200112393811234", ((string)i).c_str());
+  ASSERT_STREQ("83947238473208947234723984732849023784023", ((string)j).c_str());
+  ASSERT_STREQ("96292917385554626134779518932961417595257", ((string)l).c_str());
+  HugeInt k = HugeInt(3);
+  ASSERT_STREQ("96292917385554626134779518932961417595260", ((string)(i + j + k)).c_str());
+  ASSERT_STREQ("96292917385554626134779518932961417595260", ((string)(i + j + 3)).c_str());
+}
+
+
+TEST(HugeInt, Subtraction)
+{
+  HugeInt j("83947238473208947234723984732849023784023");
+  HugeInt i("12345678912345678900055534200112393811234");
+  HugeInt l = (j - i);
+  ASSERT_STREQ("83947238473208947234723984732849023784023", ((string)j).c_str());
+  ASSERT_STREQ("12345678912345678900055534200112393811234", ((string)i).c_str());
+  ASSERT_STREQ("71601559560863268334668450532736629972789", ((string)l).c_str());
+  // Underflow case
+  ASSERT_STREQ("0", ((string)(i - j)).c_str());
+}
+
+TEST(HugeInt, Size)
+{
+  HugeInt i(32423432);
+  EXPECT_EQ(8, i.size());
+  HugeInt j(0);
+  EXPECT_EQ(1, j.size());
 }
