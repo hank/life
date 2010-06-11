9913f5a82d1264fe9f1a7bcd7597a7ae3d21bf8a
Erik Gregg <ralree@gmail.com>
2010-06-11 10:13:00 -0400

    More poker work

--- school/605.404.OO.Programming.With.Cpp/HW2/poker.h
+++ school/605.404.OO.Programming.With.Cpp/HW2/poker.h
@@ -7,7 +7,7 @@
 #include <stdint.h>
 
 // CardValues
-enum CardValues
+enum CardNumericValues
 {
   TWO, THREE, FOUR, FIVE, SIX,
   SEVEN, EIGHT, NINE, TEN,
@@ -30,16 +30,19 @@ public:
 
   // Default constructor
   Card()
+    : value(0)
   {
     // Empty
+    // Default is the 2 of spades (0)
   }
-  // Sets card to a value, usually using enums CardValues and Suits
-  // The sum of the Suit and the CardValue is our value
+  // Sets card to a value, usually using enums CardNumericValues and Suits
+  // The sum of the Suit and the CardNumericValue is our value
   Card(uint8_t value)
   {
     this->value = value;
   }
 
+  // Alternative 2-param constructor does the addition for you.
   Card(uint8_t value, uint8_t suit)
   {
     this->value = value + suit;
@@ -51,9 +54,20 @@ public:
     this->value = card.value;
   }
 
+  // Equality
+  // Only consider numeric values
+  // value % 13 erases suits.
+  bool operator == (const Card& card) const
+  {
+    return (this->value % 13) == (card.value % 13);
+  }
+
+  // Less Than
+  // Only consider numeric values
+  // value % 13 erases suits.
   bool operator < (const Card& card) const
   {
-    return this->value < card.value;
+    return (this->value % 13) < (card.value % 13);
   }
 
   operator int() const
@@ -131,6 +145,14 @@ public:
     }
     return s;
   }
+
+  // Precedes
+  // Checks to see if this card comes right before the next
+  // with regard to numeric value.
+  bool precedes(Card& card)
+  {
+    return (this->value % 13) == ((int32_t)card.value % 13) - 1;
+  }
 private:
   // Each card is just an unsigned integer from 0 to 51.
   int value;
@@ -142,7 +164,7 @@ public:
   // Hand ranks, in order of awesomeness.
   enum HandRanking
   {
-    HIGH_CARD,
+    HIGH_CARD = 0,
     PAIR,
     TWO_PAIR,
     THREE_OF_A_KIND,
@@ -161,7 +183,9 @@ public:
     this->cards[3] = card4;
     this->cards[4] = card5;
     // Sort our hand so we can calculate its rank
+    // Use binary predicate to just sort on CardValue and not Suit
     std::sort(this->cards, this->cards + 5);
+    // Since we're sorted, the high card is at the end.
     // Calculate the rank of our hand
     calculateRank();
   }
@@ -185,9 +209,105 @@ private:
   // Calculates the hand rank based on the cards in hand.
   void calculateRank()
   {
-    // Pair
+    checkPairs();
+    checkStraight();
+    //checkFlush();
+  }
+
+  // Flush
+  // See if we have 5 of a given suit.
+  // Possible outcomes: STRAIGHT, STRAIGHT FLUSH
+  void checkFlush()
+  {
+    for(int i = 1; i < numCards; i++)
+    {
+      // Compare this card to the next.
+      if(this->cards[0].getSuit() == this->cards[i])
+      {
+        // We've failed to make a straight.
+        ++inARow;
+      }
+    }
+  }
+
+  // Straight
+  // Just compare numeric value of this card to the next
+  // Use equality operator, which compares just values
+  // If it's always increasing by one, we've got a straight.
+  // Possible outcomes: STRAIGHT
+  void checkStraight()
+  {
+    uint8_t inARow = 0; // Number of cards we've seen in a row
     for(int i = 0; i < numCards - 1; i++)
     {
+      // Compare this card to the next.
+      if(this->cards[i].precedes(this->cards[i + 1]))
+      {
+        // We've found another one in order
+        ++inARow;
+      }
+    }
+    // If all 4 comparisons succeed, we have a straight
+    if(4 == inARow)
+    {
+      this->rank = STRAIGHT;
+    }
+  }
+
+  // Pair
+  // Just compare numeric value of this card to the next
+  // Use equality operator, which compares just values
+  // Possible outcomes: HIGH_CARD, PAIR, THREE_OF_A_KIND,
+  //   TWO_PAIR, FOUR_OF_A_KIND, FULL_HOUSE
+  void checkPairs()
+  {
+    for(int i = 0; i < numCards - 1; i++)
+    {
+      if(cards[i] == cards[i+1])
+      {
+        // We found a pair.  Check if it's 3 of a kind
+        if(i + 2 < numCards - 1 && cards[i + 2] == cards[i])
+        {
+          // We found three-of-a-kind!  Check for four-of-a-kind.
+          if(i + 3 < numCards - 1 && cards[i + 3] == cards[i])
+          {
+            // 4-of-a-kind!
+            this->rank = FOUR_OF_A_KIND;
+          }
+          else
+          {
+            // Either there's no more cards or we didn't add to our pair.
+            // If we're already ranked a pair, full house!
+            if(PAIR == this->rank)
+            {
+              this->rank == FULL_HOUSE;
+            }
+            else
+            {
+              this->rank = THREE_OF_A_KIND;
+            }
+          }
+        }
+        else
+        {
+          // Either there's no more cards or we didn't add to our pair.
+          // If we're already ranked a pair
+          if(PAIR == this->rank)
+          {
+            this->rank == TWO_PAIR;
+          }
+          // It could be a three-of-a-kind-first full house!
+          else if(THREE_OF_A_KIND == this->rank)
+          {
+            this->rank = FULL_HOUSE;
+          }
+          // Otherwise, we just have one pair
+          else
+          {
+            this->rank = PAIR;
+          }
+        }
+      }
     }
   }
   // We've got 5 cards
@@ -195,7 +315,6 @@ private:
                      
   HandRanking rank; // Holds the rank of the hand
   Card cards[numCards]; // Holds cards for the hand
-  uint8_t highCardIndex; // Index of the high card
 };
 // Inlines
 #endif // POKER_HAND
--- school/605.404.OO.Programming.With.Cpp/HW2/test.cpp
+++ school/605.404.OO.Programming.With.Cpp/HW2/test.cpp
@@ -10,7 +10,7 @@ int main()
   // Initialize Seven High Straight
   Card card1 = Card(SEVEN + HEARTS);
   Card card2 = Card(SIX, HEARTS);
-  Card card3 = Card(FIVE + HEARTS);
+  Card card3 = Card(FIVE + SPADES);
   Card card4 = Card(FOUR + HEARTS);
   Card card5 = Card(THREE + HEARTS);
   PokerHand p(card1, card2, card3, card4, card5);
