4eca583c31903cda29d5a0928e42cd323d1a47ce
Erik Gregg <ralree@gmail.com>
2010-11-30 21:51:19 -0500

    Changed Owns to have update/delete cascade

--- school/605.441.Databases/project/sql/schema.sql
+++ school/605.441.Databases/project/sql/schema.sql
@@ -2,15 +2,15 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `ralreein_5charts` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `ralreein_5charts` ;
+CREATE SCHEMA IF NOT EXISTS `5charts` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `5charts` ;
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`User`
+-- Table `5charts`.`User`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`User` ;
+DROP TABLE IF EXISTS `5charts`.`User` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`User` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`User` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `first_name` VARCHAR(45) NOT NULL ,
@@ -25,11 +25,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Stock`
+-- Table `5charts`.`Stock`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Stock` ;
+DROP TABLE IF EXISTS `5charts`.`Stock` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Stock` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Stock` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `ticker` VARCHAR(8) NOT NULL ,
   `name` VARCHAR(255) NULL ,
@@ -40,11 +40,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Owns`
+-- Table `5charts`.`Owns`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Owns` ;
+DROP TABLE IF EXISTS `5charts`.`Owns` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Owns` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Owns` (
   `User_id` INT NOT NULL ,
   `Stock_id` INT NOT NULL ,
   `date_added` DATETIME NOT NULL ,
@@ -54,23 +54,23 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Owns` (
   INDEX `fk_User_has_Stock_Stock1` (`Stock_id` ASC) ,
   CONSTRAINT `fk_User_has_Stock_User`
     FOREIGN KEY (`User_id` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `5charts`.`User` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_User_has_Stock_Stock1`
     FOREIGN KEY (`Stock_id` )
-    REFERENCES `ralreein_5charts`.`Stock` (`id` )
+    REFERENCES `5charts`.`Stock` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`OHLC`
+-- Table `5charts`.`OHLC`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`OHLC` ;
+DROP TABLE IF EXISTS `5charts`.`OHLC` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`OHLC` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`OHLC` (
   `Stock_id` INT NOT NULL ,
   `date` DATETIME NOT NULL ,
   `open` DECIMAL(9,3) NOT NULL ,
@@ -81,18 +81,18 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`OHLC` (
   PRIMARY KEY (`Stock_id`, `date`) ,
   CONSTRAINT `fk_OHLC_Stock1`
     FOREIGN KEY (`Stock_id` )
-    REFERENCES `ralreein_5charts`.`Stock` (`id` )
+    REFERENCES `5charts`.`Stock` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Comment`
+-- Table `5charts`.`Comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Comment` ;
+DROP TABLE IF EXISTS `5charts`.`Comment` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Comment` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Comment` (
   `Owns_Stock_id` INT NOT NULL ,
   `Owns_User_id` INT NOT NULL ,
   `text` TEXT NOT NULL ,
@@ -102,18 +102,18 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Comment` (
   INDEX `fk_Comment_Owns1` (`Owns_Stock_id` ASC, `Owns_User_id` ASC) ,
   CONSTRAINT `fk_Comment_Owns1`
     FOREIGN KEY (`Owns_Stock_id` , `Owns_User_id` )
-    REFERENCES `ralreein_5charts`.`Owns` (`Stock_id` , `User_id` )
+    REFERENCES `5charts`.`Owns` (`Stock_id` , `User_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Feed`
+-- Table `5charts`.`Feed`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Feed` ;
+DROP TABLE IF EXISTS `5charts`.`Feed` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Feed` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Feed` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NULL ,
   `link` VARCHAR(45) NULL ,
@@ -122,11 +122,11 @@ ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Prediction`
+-- Table `5charts`.`Prediction`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Prediction` ;
+DROP TABLE IF EXISTS `5charts`.`Prediction` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Prediction` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Prediction` (
   `User_id` INT NOT NULL ,
   `Stock_id` INT NOT NULL ,
   `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
@@ -136,23 +136,23 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Prediction` (
   INDEX `fk_Prediction_Stock1` (`Stock_id` ASC) ,
   CONSTRAINT `fk_Prediction_User1`
     FOREIGN KEY (`User_id` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Prediction_Stock1`
     FOREIGN KEY (`Stock_id` )
-    REFERENCES `ralreein_5charts`.`Stock` (`id` )
+    REFERENCES `5charts`.`Stock` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`User_Feed`
+-- Table `5charts`.`User_Feed`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`User_Feed` ;
+DROP TABLE IF EXISTS `5charts`.`User_Feed` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`User_Feed` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`User_Feed` (
   `User_id` INT NOT NULL ,
   `link_hash` VARCHAR(128) NOT NULL ,
   `link` VARCHAR(768) NOT NULL ,
@@ -161,18 +161,18 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`User_Feed` (
   INDEX `fk_User_Feed_User1` (`User_id` ASC) ,
   CONSTRAINT `fk_User_Feed_User1`
     FOREIGN KEY (`User_id` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Sector`
+-- Table `5charts`.`Sector`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Sector` ;
+DROP TABLE IF EXISTS `5charts`.`Sector` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Sector` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Sector` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
@@ -181,34 +181,34 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Sector_has_Stock`
+-- Table `5charts`.`Sector_has_Stock`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Sector_has_Stock` ;
+DROP TABLE IF EXISTS `5charts`.`Sector_has_Stock` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Sector_has_Stock` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Sector_has_Stock` (
   `Sector_id` INT NOT NULL ,
   `Stock_id` INT NOT NULL ,
   PRIMARY KEY (`Sector_id`, `Stock_id`) ,
   INDEX `fk_Sector_has_Stock_Stock1` (`Stock_id` ASC) ,
   CONSTRAINT `fk_Sector_has_Stock_Sector1`
     FOREIGN KEY (`Sector_id` )
-    REFERENCES `ralreein_5charts`.`Sector` (`id` )
+    REFERENCES `5charts`.`Sector` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Sector_has_Stock_Stock1`
     FOREIGN KEY (`Stock_id` )
-    REFERENCES `ralreein_5charts`.`Stock` (`id` )
+    REFERENCES `5charts`.`Stock` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Friend`
+-- Table `5charts`.`Friend`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Friend` ;
+DROP TABLE IF EXISTS `5charts`.`Friend` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Friend` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Friend` (
   `User_from` INT NOT NULL ,
   `User_to` INT NOT NULL ,
   `added` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
@@ -216,23 +216,23 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Friend` (
   INDEX `fk_Friend_User2` (`User_to` ASC) ,
   CONSTRAINT `fk_Friend_User1`
     FOREIGN KEY (`User_from` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Friend_User2`
     FOREIGN KEY (`User_to` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Recently_Viewed_Users`
+-- Table `5charts`.`Recently_Viewed_Users`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Recently_Viewed_Users` ;
+DROP TABLE IF EXISTS `5charts`.`Recently_Viewed_Users` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Recently_Viewed_Users` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Recently_Viewed_Users` (
   `User_from` INT NOT NULL ,
   `User_to` INT NOT NULL ,
   `date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
@@ -240,23 +240,23 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Recently_Viewed_Users` (
   INDEX `fk_Recently_Viewed_Stocks_User2` (`User_to` ASC) ,
   CONSTRAINT `fk_Recently_Viewed_Stocks_User1`
     FOREIGN KEY (`User_from` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Recently_Viewed_Stocks_User2`
     FOREIGN KEY (`User_to` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = MyISAM;
 
 
 -- -----------------------------------------------------
--- Table `ralreein_5charts`.`Recently_Viewed_Stocks`
+-- Table `5charts`.`Recently_Viewed_Stocks`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ralreein_5charts`.`Recently_Viewed_Stocks` ;
+DROP TABLE IF EXISTS `5charts`.`Recently_Viewed_Stocks` ;
 
-CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Recently_Viewed_Stocks` (
+CREATE  TABLE IF NOT EXISTS `5charts`.`Recently_Viewed_Stocks` (
   `User_id` INT NOT NULL ,
   `Stock_id` INT NOT NULL ,
   `date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
@@ -264,12 +264,12 @@ CREATE  TABLE IF NOT EXISTS `ralreein_5charts`.`Recently_Viewed_Stocks` (
   INDEX `fk_Recently_Viewed_Stocks_Stock1` (`Stock_id` ASC) ,
   CONSTRAINT `fk_Recently_Viewed_Stocks_User10`
     FOREIGN KEY (`User_id` )
-    REFERENCES `ralreein_5charts`.`User` (`id` )
+    REFERENCES `5charts`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Recently_Viewed_Stocks_Stock10`
     FOREIGN KEY (`Stock_id` )
-    REFERENCES `ralreein_5charts`.`Stock` (`id` )
+    REFERENCES `5charts`.`Stock` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = MyISAM;
