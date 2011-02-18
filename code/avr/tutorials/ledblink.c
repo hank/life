ca4cabce59446fbc17b2f65dd9b607b1625d7733
Erik Gregg <ralree@gmail.com>
2011-02-18 03:03:53 -0500

    Using timers in the ATTiny85

--- code/avr/tutorials/ledblink.c
+++ code/avr/tutorials/ledblink.c
@@ -10,22 +10,26 @@
 
 #define SET_LOW(port, pin)  port &= ~_BV(pin)
 #define SET_HIGH(port, pin) port |=  _BV(pin)
-
-void sleep(uint8_t millisec)
-{
-    while(millisec--) _delay_ms(1);
-}
+#define TOGGLE(port, pin)   port ^=  _BV(pin)
 
 int main()
 {
+    uint16_t elapsed = 0;
+    // Turn on LED output pin
     SET_HIGH(DDRB, PB2);
-    while(1)
-    {
-        SET_LOW(PORTB, PB2);
-        sleep(1000);
 
-        SET_HIGH(PORTB, PB2);
-        sleep(1000);
+    // Set timer to count with F_CPU
+    SET_HIGH(TCCR0B, CS10);
+    // Set timer prescale to 101 (F/1024)
+    TCCR0B |= _BV(CS00) | _BV(CS02);
+    for(;;)
+    {
+        if(TCNT0 == 254)
+        {
+            TCNT0 = 0;
+            ++elapsed;
+            if(elapsed % 10 == 0) TOGGLE(PORTB, PB2);
+        }
     }
     return 0;
 }
