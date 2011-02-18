e4c683d3e901ae734d6748865dabd09096dee933
Erik Gregg <ralree@gmail.com>
2011-02-18 15:03:12 -0500

    Almost completed pure hardware blink

--- code/avr/include/basic_functions.h
+++ code/avr/include/basic_functions.h
@@ -1,6 +1,8 @@
 /* Basic AVR Functions 
    Makes writing code for the AVR more pleasant
 */
+
+// Basic Bitwise Functions
 #define SET_LOW(port, pin)  port &= ~_BV(pin)
 #define SET_HIGH(port, pin) port |=  _BV(pin)
 #define TOGGLE(port, pin)   port ^=  _BV(pin)
--- code/avr/tutorials/ctc_ledblink.c
+++ code/avr/tutorials/ctc_ledblink.c
@@ -2,6 +2,7 @@
  */
 #ifndef F_CPU
     // A reasonable default clock speed
+    #warning No F_CPU Defined, using 8Mhz
     #define F_CPU 8000000UL
 #endif
 
@@ -12,25 +13,25 @@
 
 int main()
 {
-    uint16_t elapsed = 0;
-    // Turn on LED output pin
-    SET_HIGH(DDRB, PB2);
+    // Enable external OC0A
+    SET_HIGH(DDRB, DDB0);
 
     // Set timer to count with F_CPU
     SET_HIGH(TCCR0B, CS10);
 
     // Turn on CTC
     SET_WGM_85(TCCR0, 0, 1, 0);
+    OCR0A = 50;
+
+    // Set toggle of OC0A on compare match
+    SET_HIGH(TCCR0A, COM0A0);
+    SET_HIGH(TCCR0A, COM0A1);
+
     // Set timer prescale to 101 (F/1024)
-    TCCR0B |= _BV(CS00) | _BV(CS02);
-    for(;;)
-    {
-        if(TCNT0 == 254)
-        {
-            TCNT0 = 0;
-            ++elapsed;
-            if(elapsed % 10 == 0) TOGGLE(PORTB, PB2);
-        }
-    }
+    TCCR0B |= _BV(CS02) | _BV(CS00);
+
+    // Loop Forever
+    for(;;);
+
     return 0;
 }
