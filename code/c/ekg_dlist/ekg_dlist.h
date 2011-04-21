9b600ee192187c1884e00d3c9dfe57d891d5e209
E G <hank@davros.(none)>
2011-04-21 18:42:23 -0400

    Adding some comments to dlist

--- code/c/ekg_dlist/ekg_dlist.h
+++ code/c/ekg_dlist/ekg_dlist.h
@@ -20,9 +20,21 @@ typedef struct {
   char * name;
 } dlist_t;
 
+// Initialize a new list
+// Returns: Pointer to the newly allocated list
+//          User must free
 dlist_t * dlist_init();
+
+// Push an element onto the tail of the list
+// Returns: The new node
 dnode_t * dlist_push(dlist_t * list, dnode_t * node);
+
+// Prints a list, in order
+// Returns the number of elements printed
 uint32_t dlist_print(dlist_t * list);
+
+// Reverses the list, in place.
+// Returns a pointer to the reversed list
 dlist_t * dlist_reverse(dlist_t * list);
 
 #endif
