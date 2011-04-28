23c7c2502c469d1644363982e332c0416b4060ce
E G <hank@davros.(none)>
2011-04-28 17:47:32 -0400

    Finished merge sort.  I am a merge master.

--- code/interview_practice/merge_sort.c
+++ code/interview_practice/merge_sort.c
@@ -5,6 +5,7 @@
 
 typedef struct _lnode {
     struct _lnode* next;
+    struct _lnode* prev;
     int data;
 } lnode;
 
@@ -21,6 +22,7 @@ void insert(list* l, int item)
     else 
     {
         l->tail->next = calloc(1,sizeof(lnode));
+        l->tail->next->prev = l->tail;
         l->tail = l->tail->next;
     }
     l->tail->data = item;
@@ -48,17 +50,47 @@ void list_free(list* l)
     }
 }
 
-void ms(lnode* b, lnode* e, int size)
+void swap(list* l, lnode* a, lnode* b)
 {
+  printf("Swapping %d and %d\n", a->data, b->data);
+  lnode* pa, *pb, *ca, *cb;
+  pa = a->prev;
+  pb = b->prev;
+  ca = a->next;
+  cb = b->next;
+  // Swap a and b's pointers out
+  a->prev = pb;
+  b->prev = pa;
+  a->next = cb;
+  b->next = ca;
+  // Set the previous and next node ptrs into a and b
+  if(pb) pb->next = a;
+  if(pa) pa->next = b;
+  if(cb) cb->prev = a;
+  if(ca) ca->prev = b;
+  // Change the list head/tail if necessary
+  if(a == l->head) l->head = b;
+  else if(b == l->head) l->head = a;
+  if(a == l->tail) l->tail = b;
+  else if(b == l->tail) l->tail = a;
+}
+
+void ms(list* l, lnode* b, lnode* e, int size)
+{
+    if(!b || !e) return;
     if(size == 2)
     {
         // Normal final case, swap if needed
-        //if(b->)
+        if(b->data > e->data) swap(l, b, e);
     }
     else
     {
         // Split, recurse
-        int 
+        int a = size/2;
+        lnode* newe = b;
+        while(a--) newe = newe->next;
+        ms(l, b, newe, size/2);
+        ms(l, newe->next, e, size/2);
     }
 }
 
@@ -70,10 +102,13 @@ int main()
     insert(l, -17);
     insert(l, 45);
     insert(l, 923);
+    printf("Unsorted: \n");
     print_list(l);
 
     // Merge sort the list
-    ms(l->head, l->tail, l->size);
+    ms(l, l->head, l->tail, l->size);
+    printf("Sorted: \n");
+    print_list(l);
 
     list_free(l);
     free(l);
