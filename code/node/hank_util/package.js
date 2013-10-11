c4b7b433cc1ec00158a27939f4c8981783797206
Hank <hank@crow.local>
2013-10-11 13:12:03 -0400

    Adding util

---
+++ code/node/hank_util/package.js
@@ -0,0 +1,17 @@
+{
+  "author": {
+    "name": "Erik Gregg",
+    "email": "ralree@gmail.com",
+  },
+  "name": "hank_util",
+  "description": "Random utilities",
+  "version": "1.0.0",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/hank/life/code/node/hank_util"
+  },
+  "main": "hank_util.js",
+  "license": {
+    "type": "Public Domain"
+  }
+}
