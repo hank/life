b5d0c88452a4f7ece6d333135a1452f440b7a326
hank <hank@jointsecurityarea.org>
2014-07-01 21:57:40 -0400

    Making dice for bitcoin script better

--- code/python/bitcoin-dice-gen/gen.py
+++ code/python/bitcoin-dice-gen/gen.py
@@ -1,8 +1,13 @@
-import binascii, argparse, struct, sys
+import binascii, argparse, struct, sys, math
+from collections import Counter
 
 def int2bin(n, count):
     """returns the binary of integer n, using count number of digits"""
     return "".join([str((n >> y) & 1) for y in range(count-1, -1, -1)])
+
+def entropy(s):
+    p, lns = Counter(s), float(len(s))
+    return -sum( count/lns * math.log(count/lns, 2) for count in p.values())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -16,15 +21,23 @@ if __name__ == "__main__":
         g = int(g / 2)
         h += 1
     h = int(h)
-    print "{} is the divisor".format(2**h)
+    cutoff = 2**h
+    print "{} is the cutoff, re-roll larger values".format(cutoff)
     rolls = ""
     rolls_required = 256/h
     print "Input {} dice rolls:".format(rolls_required)
     while rolls_required > 0:
         try:
             i = raw_input("Roll ({} left): ".format(rolls_required))
-            print "Adding {} bits from {}".format(h, (int(i) % 2**h))
-            binform = int2bin(int(i) % 2**h, h)
+            i = int(i)
+            if i > cutoff:
+                print "Fail - you need to throw away values larger than {}".format(cutoff)
+                continue
+            elif i == 0:
+                print "0 is not a valid dice roll!"
+                continue
+            print "Adding {} bits from {}".format(h, i)
+            binform = int2bin(i-1, h)
             rolls += binform
             print rolls
             rolls_required -= 1
@@ -36,3 +49,4 @@ if __name__ == "__main__":
     # Turn into a key
     key = int(rolls, 2)
     print hex(key)[2:-1].upper()
+    print "Entropy: {}".format(entropy(rolls))
