a72cc46fbb6b0769c07d4b4413994ce7d42ef510
Erik Gregg <erik@greggindustri.es>
2017-09-19 00:09:02 -0400

    Adding script to export google forms data

---
+++ code/python/docs/export_form.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Google Drive API.
+
+    Creates a Google Drive API service object and outputs the names and IDs
+    for up to 10 files.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    results = service.files().list(
+        pageSize=10,fields="nextPageToken, files(id, name, mimeType)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+    print(items[0])
+    results = service.files().export(fileId=items[0]['id'], mimeType='text/csv').execute()
+    # print(results)
+    import csv
+    import io
+    csvIn = io.StringIO(unicode(results))
+    reader = csv.reader(csvIn)
+    reader = list(reader)
+    labels = reader[0]
+    for row in reader[1:]:
+        # Open a file with the address as the name and add fields to it
+        fname = (row[labels.index('Inspection Address')].replace(r' ', '-') + ".txt")
+        print("Writing {}".format(fname))
+        with open(fname, 'w') as f:
+            for idx, field in enumerate(row):
+                f.write("{}: {}\n".format(labels[idx], field))
+                # print("{}: {}".format(labels[idx], field))
+
+if __name__ == '__main__':
+    main()
+
